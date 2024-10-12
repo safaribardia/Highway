ece3a95fdaaf7266f0de33fa66df1371926ae7b1
BardiaSafari <contact.bardia.safari@gmail.com>
2024-10-12 11:02:18 -0700

    Add necessary middleware

--- highway-backend/index.js
+++ highway-backend/index.js
@@ -1,10 +1,19 @@
 require("express-async-errors")
 const express = require("express")
+const errorHandler = require("./middleware/errorHandler");
 const logger = require("./logging/logger");
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const dotenv = require('dotenv');
 
-const app = express()
+const app = express();
 
 app.use(express.json());
+app.use(errorHandler);
+app.use(helmet());
+app.use(compression());
+app.use(morgan('tiny'));
 
 const PORT = process.env.PORT | 3001;
 app.listen(PORT, () => logger.info("Listening " + PORT));
