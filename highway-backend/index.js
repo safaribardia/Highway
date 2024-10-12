c485a305c58b68858012058819c153bb130a5ad9
BardiaSafari <contact.bardia.safari@gmail.com>
2024-10-12 10:43:10 -0700

    Add error logging

--- highway-backend/index.js
+++ highway-backend/index.js
@@ -1,8 +1,10 @@
 require("express-async-errors")
 const express = require("express")
-const logger = require("./logger");
+const logger = require("./logging/logger");
 
 const app = express()
+
+app.use(express.json());
 
 const PORT = process.env.PORT | 3001;
 app.listen(PORT, () => logger.info("Listening " + PORT));
